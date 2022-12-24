976c93dfa775cbf003c672b0e3518c17ddc6bf7a
nike <vavilovnv@gmail.com>
2022-12-24 18:35:48 +0300

    Auth and chat ability added.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import asyncio
 from asyncio.streams import StreamReader, StreamWriter
+from datetime import datetime
 from utils import BYTES, HOST, PORT, get_logger
 
 logger = get_logger()
@@ -9,6 +10,8 @@ class Server:
     def __init__(self, host: str = HOST, port: int = PORT):
         self.host, self.port = host, port
         self.clients = {}
+        self.users_auth_data = {}
+        self.history = []
 
     async def write_to_client(self, address: str, message: str):
         writer = self.clients[address][1]
@@ -23,37 +26,94 @@ class Server:
             if not data:
                 logger.error("Can't read message from client %s", address)
                 return answer
-            answer = data.decode()
+            answer = data.decode().strip()
         except Exception as error:
             logger.error(error)
             return answer
         return answer
 
-    def registration_new_client(self, address: str, reader_writer: tuple):
+    def add_new_client(self, address: str, reader_writer: tuple):
         self.clients[address] = reader_writer
 
-    async def welcome_new_client(self, address: str) -> str:
-        await self.write_to_client(address, 'You are welcome!\n')
+    async def get_auth_data(self, address: str, new_user: bool = False) -> tuple:
+        while True:
+            await self.write_to_client(address, 'Input your login: ')
+            login = await self.read_from_client(address)
+            if login:
+                if new_user and login in self.users_auth_data:
+                    await self.write_to_client(address, 'The login is taken. Input another login.\n')
+                    continue
+                await self.write_to_client(address, 'Input your password: ')
+                password = await self.read_from_client(address)
+                break
+        return login, password
+
+    async def authorize_new_user(self, address: str) -> str:
+        while True:
+            login, password = await self.get_auth_data(address, new_user=True)
+            await self.write_to_client(address, 'Login and password was set.\n')
+            self.users_auth_data[login] = password
+            break
+        return login
+
+    async def login_user(self, address: str) -> str:
+        is_authorized = False
+        while not is_authorized:
+            login, password = await self.get_auth_data(address)
+            if login not in self.users_auth_data:
+                await self.write_to_client(address, 'User not found.\n')
+                return ''
+            if password != self.users_auth_data[login]:
+                await self.write_to_client(address, 'Wrong password.\n')
+                return ''
+            is_authorized = True
+            await self.write_to_client(address, 'Login successful.\n')
+        return login
+
+    async def user_authorization(self, address: str) -> str:
+        await self.write_to_client(address, 'Please, register (/auth) or log in (/login).\n')
         while True:
             answer = await self.read_from_client(address)
             if answer == '/auth':
-                login = ''
+                login = await self.authorize_new_user(address)
                 break
-            elif answer == '/signin':
-                login = ''
-                break
-            await self.write_to_client(address, 'Command unknown, please repeat.')
+            elif answer == '/login':
+                login = await self.login_user(address)
+                if login:
+                    break
+                continue
+            await self.write_to_client(address, 'Command unknown, please repeat.\n')
         return login
 
-    async def new_connections(self, reader: StreamReader, writer: StreamWriter) -> None:
+    async def send_to_all(self, message, login, address):
+        for adr in self.clients:
+            time_now = datetime.now()
+            if adr == address:
+                msg = f'{time_now} me says: {message}\n'
+            else:
+                msg = f'{time_now} {login} says: {message}\n'
+            await self.write_to_client(adr, msg)
+
+    async def new_connection(self, reader: StreamReader, writer: StreamWriter) -> None:
         ip, port = writer.get_extra_info('peername')
         address = f'{ip}:{port}'
         logger.info('New client connected from %s:%s', ip, port)
-        self.registration_new_client(address, (reader, writer))
-        await self.welcome_new_client(address)
+        self.add_new_client(address, (reader, writer))
+        login = await self.user_authorization(address)
+        await self.write_to_client(address, 'You are in general chat.\n')
+        while True:
+            message = await self.read_from_client(address)
+            if message == '/exit':
+                await self.write_to_client(address, 'You are disconnected from chat. Have a nice day.\n')
+                writer.close()
+                logger.info('User %s at %s disconnected.', login, address)
+                break
+            else:
+                self.history.append(message)
+                await self.send_to_all(message, login, address)
 
     async def run_server(self) -> None:
-        server_instance = await asyncio.start_server(self.new_connections, self.host, self.port)
+        server_instance = await asyncio.start_server(self.new_connection, self.host, self.port)
         logger.info('Server running at %s:%s', self.host, self.port)
         async with server_instance:
             await server_instance.serve_forever()

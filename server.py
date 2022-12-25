8484fde5cb14356c2e9da3137770da86416b8475
nike <vavilovnv@gmail.com>
2022-12-25 19:58:17 +0300

    Server engine in progress.

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 import asyncio
 from asyncio.streams import StreamReader, StreamWriter
 from datetime import datetime
-from utils import BYTES, HOST, PORT, get_logger
+from utils import BYTES, HOST, PORT, get_logger, format_message
 
 logger = get_logger()
 
 
 class Server:
-    def __init__(self, host: str = HOST, port: int = PORT):
+    def __init__(self, host: str = HOST, port: int = PORT, short_history_depth: int = 20):
         self.host, self.port = host, port
         self.clients = {}
         self.users_auth_data = {}
         self.history = []
+        self.short_history_depth = short_history_depth
 
-    async def write_to_client(self, address: str, message: str):
+    async def write_to_client(self, address: str, message: str, line_break: bool = True) -> None:
         writer = self.clients[address][1]
+        message += '\n' if line_break else ''
         writer.write(message.encode())
         await writer.drain()
 
@@ -36,14 +38,15 @@ class Server:
         self.clients[address] = reader_writer
 
     async def get_auth_data(self, address: str, new_user: bool = False) -> tuple:
+        login, password = '', ''
         while True:
-            await self.write_to_client(address, 'Input your login: ')
+            await self.write_to_client(address, 'Input your login: ', line_break=False)
             login = await self.read_from_client(address)
             if login:
                 if new_user and login in self.users_auth_data:
-                    await self.write_to_client(address, 'The login is taken. Input another login.\n')
+                    await self.write_to_client(address, 'The login is taken. Input another login.')
                     continue
-                await self.write_to_client(address, 'Input your password: ')
+                await self.write_to_client(address, 'Input your password: ', line_break=False)
                 password = await self.read_from_client(address)
                 break
         return login, password
@@ -51,28 +54,30 @@ class Server:
     async def authorize_new_user(self, address: str) -> str:
         while True:
             login, password = await self.get_auth_data(address, new_user=True)
-            await self.write_to_client(address, 'Login and password was set.\n')
-            self.users_auth_data[login] = password
+            await self.write_to_client(address, 'Login and password was set.')
+            self.users_auth_data[login] = password, [address]
             break
         return login
 
     async def login_user(self, address: str) -> str:
-        is_authorized = False
+        login, is_authorized = '', False
         while not is_authorized:
             login, password = await self.get_auth_data(address)
             if login not in self.users_auth_data:
-                await self.write_to_client(address, 'User not found.\n')
+                await self.write_to_client(address, 'User not found.')
                 return ''
             if password != self.users_auth_data[login]:
-                await self.write_to_client(address, 'Wrong password.\n')
+                await self.write_to_client(address, 'Wrong password.')
                 return ''
             is_authorized = True
-            await self.write_to_client(address, 'Login successful.\n')
+            if address not in self.users_auth_data[login][1]:
+                self.users_auth_data[login][1].append(address)
+            await self.write_to_client(address, 'Login successful.')
         return login
 
     async def user_authorization(self, address: str) -> str:
-        await self.write_to_client(address, 'Please, register (/auth) or log in (/login).\n')
         while True:
+            await self.write_to_client(address, 'Please, register (/auth) or log in (/login).')
             answer = await self.read_from_client(address)
             if answer == '/auth':
                 login = await self.authorize_new_user(address)
@@ -82,17 +87,55 @@ class Server:
                 if login:
                     break
                 continue
-            await self.write_to_client(address, 'Command unknown, please repeat.\n')
+            await self.write_to_client(address, 'Command unknown, please repeat.')
         return login
 
     async def send_to_all(self, message, login, address):
         for adr in self.clients:
-            time_now = datetime.now()
             if adr == address:
-                msg = f'{time_now} me says: {message}\n'
+                login = 'me'
+            await self.write_to_client(adr, format_message(message, login))
+
+    async def send_private(self, message, cur_login, address):
+        message = message.replace('/private', '').strip()
+        login, message = message.split(maxsplit=1)
+        if login in self.users_auth_data and login == cur_login:
+            await self.write_to_client(address, format_message(message, 'me', is_private=True))
+        elif login in self.users_auth_data and login != cur_login:
+            for adr in self.users_auth_data[login][1]:
+                await self.write_to_client(adr, format_message(message, login, is_private=True))
+        else:
+            await self.write_to_client(address, 'Wrong user login.')
+
+    async def send_short_history(self, address) -> None:
+        await self.write_to_client(address, 'You are in general chat.')
+        for msg in self.history[:self.short_history_depth]:
+            await self.write_to_client(address, msg[1])
+
+    async def close_client_connection(self, address, login):
+        writer = self.clients[address][1]
+        logger.info('User %s at %s disconnected.', login, address)
+        del self.clients[address]
+        try:
+            writer.close()
+            await writer.wait_closed()
+        except Exception as error:
+            logger.error('Ошибка при закрытии writer клиента %s %s.', address, error)
+
+    async def chat_with_client(self, address: str, login: str) -> None:
+        while message := await self.read_from_client(address):
+            if message == '/exit':
+                await self.write_to_client(address, 'You are disconnected from chat. Have a nice day.\n')
+                await self.close_client_connection(address, login)
+                break
+            elif message.startswith('/send private'):
+                await self.send_private(message, login, address)
+            elif message.startswith('/send'):
+                message = message.replace('/send', '').strip()
+                self.history.append(format_message(message, login))
+                await self.send_to_all(message, login, address)
             else:
-                msg = f'{time_now} {login} says: {message}\n'
-            await self.write_to_client(adr, msg)
+                await self.write_to_client(address, 'Wrong command.')
 
     async def new_connection(self, reader: StreamReader, writer: StreamWriter) -> None:
         ip, port = writer.get_extra_info('peername')
@@ -101,18 +144,8 @@ class Server:
         self.add_new_client(address, (reader, writer))
         login = await self.user_authorization(address)
         logger.info('User %s authorized.', login)
-        await self.write_to_client(address, 'You are in general chat.\n')
-        while True:
-            message = await self.read_from_client(address)
-            if message == '/exit':
-                await self.write_to_client(address, 'You are disconnected from chat. Have a nice day.\n')
-                writer.close()
-                logger.info('User %s at %s disconnected.', login, address)
-                del self.clients[address]
-                break
-            else:
-                self.history.append(message)
-                await self.send_to_all(message, login, address)
+        await self.send_short_history(address)
+        await self.chat_with_client(address, login)
 
     async def run_server(self) -> None:
         server_instance = await asyncio.start_server(self.new_connection, self.host, self.port)
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from datetime import datetime
 
 BYTES = 1024 * 5
 HOST = '127.0.0.1'
@@ -15,3 +16,14 @@ def get_logger() -> logging.Logger:
         format='%(name)s - %(asctime)s - %(levelname)s - %(message)s',
     )
     return logging.getLogger('chat-service')
+
+
+def format_datatime(datetime: datetime) -> str:
+    pattern = '%Y.%m.%d %H:%M:%S'
+    return datetime.strftime(pattern)
+
+
+def format_message(message: str, login: str, is_private: bool = False) -> str:
+    date_time = format_datatime(datetime.now())
+    private = 'in private' if is_private else ''
+    return f'{date_time} {login} {private} says: {message}'

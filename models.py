5023349ca8d6fe2f7da8f48b9bceb0b650a65e3c
nike <vavilovnv@gmail.com>
2022-12-27 17:14:04 +0300

    Models extended.

--- models.py
+++ models.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 from typing import Optional
+from uuid import uuid4
 
 
 class Message:
 
-    def __init__(self, text: str, login: str, is_private: bool = False, recipient: str = ''):
+    def __init__(self, text: str, login: str, is_private: bool = False, recipient: str = '', chat_name: str = ''):
         self.is_private = is_private
         self.login = login
         self.pub_date = datetime.now()
+        self.recipient = recipient
+        self.chat_name = chat_name
         self.text = self.format_message(text)
 
     def format_message(self, text: str) -> str:
@@ -23,6 +26,24 @@ class User:
         self.password: str = password
         self.addresses: list[str] = []
         self.logout_time: Optional[datetime] = None
+        self.__count_sent_message: int = 0
+        self.__pub_date: Optional[datetime] = None
+        self.private_chats: dict[str, str] = {}
+
+    @property
+    def count_sent_messages(self):
+        return self.__count_sent_message
+
+    @count_sent_messages.setter
+    def count_sent_messages(self, dt_now):
+        if (not self.__pub_date
+                or self.__pub_date.date() != dt_now.date()
+                or (self.__pub_date.date() == dt_now.date()
+                    and self.__pub_date.hour != dt_now.hour)):
+            self.__count_sent_message = 1
+        else:
+            self.__count_sent_message += 1
+        self.__pub_date = dt_now
 
 
 class Chat:
@@ -31,3 +52,9 @@ class Chat:
         self.name = name
         self.admin = admin
         self.users = []
+        self.__private_keys = {}
+
+    def get_private_key(self, login):
+        if login not in self.__private_keys:
+            self.__private_keys[login] = uuid4().hex
+        return self.__private_keys[login]

bb9c221e529500ced945f5acd7d234fa3869b0ee
nike <vavilovnv@gmail.com>
2022-12-29 19:33:24 +0300

    Server unittest extended.

--- tests/tests.py
+++ tests/tests.py
@@ -2,13 +2,20 @@ import asyncio
 import socket
 import threading
 import time
+from signal import signal, SIGPIPE, SIG_DFL
 from unittest import TestCase
 from server import Server
-from utils import BYTES, HOST, PORT, SEND_MESSAGE
+from utils import AUTH_OR_LOGIN, INPUT_LOGIN, GENERAL_CHAT, LOGIN_SET, INPUT_PASSWORD, BYTES, HOST, PORT, SEND_MESSAGE,\
+    EXIT, LOGIN_SUCCESSFUL, AUTH, LOGIN
+
+
+signal(SIGPIPE, SIG_DFL)
 
 
 class LoopRunner(threading.Thread):
-    """Вспомогательный класс для организации тестовой среды."""
+    """Вспомогательный класс для организации тестовой среды. Помогает
+    запустить и остановить сервер после тестирования."""
+
     def __init__(self, loop):
         threading.Thread.__init__(self, name='runner')
         self.loop = loop
@@ -26,18 +33,19 @@ class LoopRunner(threading.Thread):
         return res.result()
 
     def _stop(self):
-        time.sleep(0.1)
         self.loop.stop()
 
     def run_in_thread(self, callback, *args):
         return self.loop.call_soon_threadsafe(callback, *args)
 
     def stop(self):
+        time.sleep(0.3)  # костыль для завершения pending tasks
         return self.run_in_thread(self._stop())
 
 
 class TestServer(TestCase):
     """Тестирование обработки запросов сервером."""
+
     def setUp(self):
         self.loop = asyncio.new_event_loop()
         self.runner = LoopRunner(self.loop)
@@ -48,47 +56,99 @@ class TestServer(TestCase):
         self.runner.join()
 
     def test_server_methods(self):
-        server = Server(self.loop)
+        """Тестирование подключения к серверу и обработки запросов."""
+
+        # запускаем сервер с параметром -
+        # каждый пользователь может оставлять только два сообщения в час
+        server = Server(event_loop=self.loop, sent_message_per_user=2)
         self.runner.run_coroutine(server.run_server())
+        sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((HOST, PORT))
+        # проверяем первое подключение к серверу
+        sock1.connect((HOST, PORT))
+        data = sock1.recv(BYTES)
+        self.assertEqual(len(server.connections), 1)
 
-        data = sock.recv(BYTES)
-        self.assertEqual(data.decode(), 'Please, register (/auth) or log in (/login).\n')
+        # проверяем приглашение на авторизацию и создаем нового пользователя
+        self.assertEqual(data.decode(), f'{AUTH_OR_LOGIN}\n')
+        sock1.send(AUTH.encode())
+        data = sock1.recv(BYTES)
+        self.assertEqual(data.decode(), INPUT_LOGIN)
+        sock1.send('new_user'.encode())
+        data = sock1.recv(BYTES)
+        self.assertEqual(data.decode(), INPUT_PASSWORD)
+        sock1.send('password'.encode())
+        data = sock1.recv(BYTES)
 
-        sock.send('/auth'.encode())
-        data = sock.recv(BYTES)
-        self.assertEqual(data.decode(), 'Input your login: ')
-
-        sock.send('user'.encode())
-        data = sock.recv(BYTES)
-        self.assertEqual(data.decode(), 'Input your password: ')
-
-        sock.send('password'.encode())
-        data = sock.recv(BYTES)
-        self.assertEqual(data.decode(), 'Login and password was set.\n')
-        sock.close()
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((HOST, PORT))
-
-        sock.recv(BYTES)
-        sock.send('/login'.encode())
-
-        sock.recv(BYTES)
-        sock.send('user'.encode())
-
-        sock.recv(BYTES)
-        sock.send('password'.encode())
-
-        data = sock.recv(BYTES)
-        self.assertEqual(data.decode(), 'Login successful.\n')
-
-        message = f'{SEND_MESSAGE} hi!'
-        sock.send(message.encode())
-
-        self.assertEqual(len(server.history), 1)
+        # проверяем вход в чат и количество созданных юзеров
+        self.assertEqual(data.decode(), f'{LOGIN_SET}\n')
+        data = sock1.recv(BYTES)
+        self.assertEqual(data.decode(), f'{GENERAL_CHAT}\n')
         self.assertEqual(len(server.users), 1)
 
-        sock.close()
+        # проверяем второе подключение к серверу
+        sock2.connect((HOST, PORT))
+        sock2.recv(BYTES)
+        self.assertEqual(len(server.connections), 2)
+
+        # проверяем приглашение на авторизацию и логинимся под созданным
+        # ранее пользователем
+        sock2.send(LOGIN.encode())
+        sock2.recv(BYTES)
+        sock2.send('new_user'.encode())
+        sock2.recv(BYTES)
+        sock2.send('password'.encode())
+
+        # проверяем вход в чат и то, что количество пользователей осталось
+        # прежним
+        data = sock2.recv(BYTES)
+        self.assertEqual(data.decode(), f'{LOGIN_SUCCESSFUL}\n')
+        data = sock2.recv(BYTES)
+        self.assertEqual(data.decode(), f'{GENERAL_CHAT}\n')
+        self.assertEqual(len(server.users), 1)
+
+        # отправляем сообщение от второго подключения в общий чат
+        message = f'{SEND_MESSAGE} hi!'
+        sock2.send(message.encode())
+
+        # проверяем его получение первым подключением
+        data = sock1.recv(BYTES)
+        self.assertIn('says: hi!', data.decode())
+
+        # проверяем его получение вторым подключением
+        data = sock2.recv(BYTES)
+        self.assertIn('says: hi!', data.decode())
+
+        # убеждаемся что в истории сохранено одно сообщение
+        self.assertEqual(len(server.history), 1)
+
+        # отправляем второе сообщение от первого клиента и убеждаемся, что его
+        # получили оба клиента
+        message = f'{SEND_MESSAGE} hi again!'
+        sock1.send(message.encode())
+        data = sock1.recv(BYTES)
+        self.assertIn('says: hi again!', data.decode())
+        data = sock2.recv(BYTES)
+        self.assertIn('says: hi again!', data.decode())
+
+        # проверяем что в истории сохранено два сообщения
+        self.assertEqual(len(server.history), 2)
+
+        # отправляем третье сообщение, которое сервер должен отвергнуть
+        message = f'{SEND_MESSAGE} hi once more!'
+        sock2.send(message.encode())
+        data = sock2.recv(BYTES)
+        self.assertEqual(
+            data.decode(),
+            ('Sorry, but you have reached your limit of 2 per hour. '
+             'The message not be sent.\n'))
+
+        # проверяем что в истории по прежнему два сообщения
+        self.assertEqual(len(server.history), 2)
+        # time.sleep(5)
+        # отключаем клиентов
+        sock1.send(EXIT.encode())
+        sock2.send(EXIT.encode())
+        sock1.close()
+        sock2.close()
